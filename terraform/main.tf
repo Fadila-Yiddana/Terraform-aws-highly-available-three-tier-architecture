489e2ce53a646cfcce9fa04806e080045f6d0dfe
Fadila Yiddana <dilayidd10@gmail.com>
2026-06-22 22:46:16 +0000

    Add private subnets and internet gateway resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,49 @@ resource "aws_subnet" "public_b" {
     Name = "Public-Subnet-B"
   }
 }
+
+resource "aws_subnet" "private_app_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private-App-Subnet-A"
+  }
+}
+resource "aws_subnet" "private_app_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private-App-Subnet-B"
+  }
+}
+
+resource "aws_subnet" "private_db_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private-DB-Subnet-A"
+  }
+}
+resource "aws_subnet" "private_db_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private-DB-Subnet-B"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
