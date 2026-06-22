15f255cb22abe91637e44afa9c2ee64cb58f189c
Fadila Yiddana <dilayidd10@gmail.com>
2026-06-22 22:33:51 +0000

    Add VPC CIDR variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,10 @@ variable "project_name" {
   type        = string
   default     = "three-tier-architecture"
 }
+
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
