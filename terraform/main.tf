f4dc838d0c1e30671a6837dd655971008510c76d
Fadila Yiddana <dilayidd10@gmail.com>
2026-06-22 22:40:01 +0000

    Add VPC and public subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,29 @@
-# Main Infrastructure Resources
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Subnet-A"
+  }
+}
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Subnet-B"
+  }
+}
