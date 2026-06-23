0231b5ea0a45ef5c5787a50697437ad0a6ce5ddd
Fadila Yiddana <dilayidd10@gmail.com>
2026-06-23 13:56:08 +0000

    Add Terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,24 @@
-# Terraform Outputs
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_a_id" {
+  description = "Public Subnet A ID"
+  value       = aws_subnet.public_a.id
+}
+
+output "public_subnet_b_id" {
+  description = "Public Subnet B ID"
+  value       = aws_subnet.public_b.id
+}
+
+output "alb_dns_name" {
+  description = "Application Load Balancer DNS Name"
+  value       = aws_lb.main.dns_name
+}
+
+output "rds_endpoint" {
+  description = "RDS Endpoint"
+  value       = aws_db_instance.main.endpoint
+}
