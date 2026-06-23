222174d4aeaa9f60ab8e96f2411f6aa8684d1ffd
Fadila Yiddana <dilayidd10@gmail.com>
2026-06-23 13:11:14 +0000

    Add EC2 instance type variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,3 +16,10 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
