f7a54366daaa2a1700de4049570bdc150e53f844
Fadila Yiddana <dilayidd10@gmail.com>
2026-06-23 13:24:44 +0000

    Add database variables for Terraform configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,3 +23,22 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "appdb"
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
